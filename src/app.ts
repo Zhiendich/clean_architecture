5be608314ea4ebf9f0fe1310d565327d79af936b
Nikita <zhiendich@gmail.com>
2024-10-05 17:36:42 +0300

    added eslint husky prettier

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import express, { Application } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import router from "./web-api/routes/router.js";
-import { DatabaseContext } from "./infrastructure/dbContext/index.js";
-import cookieParser from "cookie-parser";
-import { logger } from "./infrastructure/logger/index.js";
-import swaggerUi from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
-import { swaggerOptions } from "./web-api/swagger.js";
-import { DIContainer } from "./infrastructure/DIContainer.js";
-import { PostgresqlDatabase } from "./infrastructure/sequelize/postgresql.js";
-import helmet from "helmet";
-import compression from "compression";
-import path from "path";
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import router from './web-api/routes/router.js';
+import { DatabaseContext } from './infrastructure/dbContext/index.js';
+import cookieParser from 'cookie-parser';
+import { logger } from './infrastructure/logger/index.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import { swaggerOptions } from './web-api/swagger.js';
+import { DIContainer } from './infrastructure/DIContainer.js';
+import { PostgresqlDatabase } from './infrastructure/sequelize/postgresql.js';
+import helmet from 'helmet';
+import compression from 'compression';
 
 dotenv.config();
 
@@ -30,20 +29,20 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(helmet());
 app.use(compression());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use("/api", router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api', router);
 
 const start = async () => {
   try {
     await db.connect().catch((error: Error) => {
-      logger.error("Connect to database error", error);
+      logger.error('Connect to database error', error);
     });
     await DIContainer.getRedisRepository().connect();
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`);
     });
   } catch (error) {
-    logger.error("Start application error", error);
+    logger.error('Start application error', error);
   }
 };
 
